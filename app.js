6a35568f8e921eb0d723807672e1cd1386b9494f
facu sacchi <faq.ndo.n.s@gmail.com>
2021-10-11 16:31:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded());
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.redirect('/home');
+});
