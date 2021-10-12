70f472e967b282398d2459dcbf903ba5635609ad
facu sacchi <faq.ndo.n.s@gmail.com>
2021-10-11 22:06:59 -0300

    agrego start en la parte de script y tmb el proccess.env.PORT en el app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded());
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server running on port 5000");
 });
 
